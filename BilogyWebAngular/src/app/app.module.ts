562864afacc42134f4cd9feaa91a94ab13f269cd
Kyoo San Martin Monroy <kyoo_16@hotmail.com>
2022-10-02 09:02:26 +0000

    Se instalo Angular Material al proyecto https://material.angular.io/components/toolbar/examples

--- BilogyWebAngular/src/app/app.module.ts
+++ BilogyWebAngular/src/app/app.module.ts
@@ -4,13 +4,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
