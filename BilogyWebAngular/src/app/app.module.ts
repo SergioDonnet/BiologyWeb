69d9ee126e4d798f456510e822ac3ec87f2853a9
Kyoo San Martin Monroy <kyoo_16@hotmail.com>
2022-10-06 04:30:35 +0000

    Cambios para agregar componentes

--- BilogyWebAngular/src/app/app.module.ts
+++ BilogyWebAngular/src/app/app.module.ts
@@ -3,21 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { MenuComponent } from './menu/menu.component';
+import { MenuButtonComponent } from './menu/menu-button/menu-button.component';
 import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatButtonModule } from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    MenuButtonComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     MatToolbarModule,
-    MatButtonModule,
     MatIconModule,
+    MatButtonModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
