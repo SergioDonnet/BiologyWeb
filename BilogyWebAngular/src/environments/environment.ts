d858e4f80404784838f29454a252a564cfbd20ce
Kyoo San Martin Monroy <kyoo_16@hotmail.com>
2022-10-10 06:28:36 +0000

    Install Firebase packages

--- BilogyWebAngular/src/environments/environment.ts
+++ BilogyWebAngular/src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    projectId: 'biologyweb-e247e',
+    appId: '1:339240525328:web:5b25157c33b9d4fbe57495',
+    storageBucket: 'biologyweb-e247e.appspot.com',
+    apiKey: 'AIzaSyCF64MKIj_FUE0qMJMmZP95Wph3NuU5-ZU',
+    authDomain: 'biologyweb-e247e.firebaseapp.com',
+    messagingSenderId: '339240525328',
+  }
 };
 
 /*
