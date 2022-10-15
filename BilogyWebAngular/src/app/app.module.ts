d0d2040d7fd11233a11047a6bd137b4e44ae13cf
SergioDonnet <donnet7534@gmail.com>
2022-10-15 19:07:02 +0000

    organizacion de componentes

--- BilogyWebAngular/src/app/app.module.ts
+++ BilogyWebAngular/src/app/app.module.ts
@@ -3,23 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { MatCardModule } from '@angular/material/card';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MenuComponent } from './menu/menu.component';
-import { MenuButtonComponent } from './menu/menu-button/menu-button.component';
+import { MenuComponent } from './components/menu/menu.component';
+import { MenuButtonComponent } from './components/menu/menu-button/menu-button.component';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { LoginComponent } from './components/login/login.component';
 import { CardsComponent } from './components/cards/cards.component';
 import { FooterComponent } from './components/footer/footer.component';
-import { NavbarComponent } from './navbar/navbar.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { environment } from '../environments/environment';
 import { provideAuth, getAuth } from '@angular/fire/auth';
 import { provideFirestore, getFirestore } from '@angular/fire/firestore';
 import { provideFunctions, getFunctions } from '@angular/fire/functions';
-import { HomePageComponent } from './navbar/home-page/home-page.component';
-import { PruebaUnoComponent } from './components/prueba-uno/prueba-uno.component';
-import { PruebaDosComponent } from './components/prueba-dos/prueba-dos.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { ProfilePageComponent } from './pages/profile-page/profile-page.component';
 
 
 @NgModule({
@@ -32,8 +32,8 @@ import { PruebaDosComponent } from './components/prueba-dos/prueba-dos.component
     FooterComponent,
     NavbarComponent,
     HomePageComponent,
-    PruebaUnoComponent,
-    PruebaDosComponent,
+    ProfileComponent,
+    ProfilePageComponent,
   ],
   imports: [
     BrowserModule,
--- BilogyWebAngular/src/app/components/profile/profile.component.spec.ts
+++ BilogyWebAngular/src/app/components/profile/profile.component.spec.ts
@@ -1,18 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { PruebaUnoComponent } from './prueba-uno.component';
+import { ProfileComponent } from './profile.component';
 
-describe('PruebaUnoComponent', () => {
-  let component: PruebaUnoComponent;
-  let fixture: ComponentFixture<PruebaUnoComponent>;
+describe('ProfileComponent', () => {
+  let component: ProfileComponent;
+  let fixture: ComponentFixture<ProfileComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ PruebaUnoComponent ]
+      declarations: [ ProfileComponent ]
     })
     .compileComponents();
 
-    fixture = TestBed.createComponent(PruebaUnoComponent);
+    fixture = TestBed.createComponent(ProfileComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
