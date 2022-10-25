8034146808c0df8f20ac9d7ebefeb84036eaf9f7
SergioDonnet <donnet7534@gmail.com>
2022-10-25 03:20:51 +0000

    se agrega imagenes al html home-page

--- BilogyWebAngular/src/app/app-routing.module.ts
+++ BilogyWebAngular/src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { AboutUsButtonComponent } from './pages/about-us-button/about-us-button.component';
 import { CoursesPageComponent } from './pages/courses-page/courses-page.component';
 import { HomePageComponent } from './pages/home-page/home-page.component';
+import { ProfilePageComponent } from './pages/profile-page/profile-page.component';
 
 const routes: Routes = [
   {path:'home', component:HomePageComponent},
   {path:'courses', component:CoursesPageComponent},
   {path:'about-us', component:AboutUsButtonComponent},
+  {path:'profile',component:ProfilePageComponent},
   {path:'**', redirectTo: 'home', pathMatch: 'full'},
   {path:'', component:HomePageComponent}
 ];
