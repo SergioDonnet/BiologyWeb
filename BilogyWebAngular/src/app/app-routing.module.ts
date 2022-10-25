49bcbba145c3956a24833bf489c14401df7dca27
GabyMarsan <anagbms07@gmail.com>
2022-10-25 03:31:54 +0000

    routing home >courses

--- BilogyWebAngular/src/app/app-routing.module.ts
+++ BilogyWebAngular/src/app/app-routing.module.ts
@@ -4,15 +4,18 @@ import { CoursesComponent } from './components/courses/courses.component';
 import { AboutUsButtonComponent } from './pages/about-us-button/about-us-button.component';
 import { CoursesPageComponent } from './pages/courses-page/courses-page.component';
 import { HomePageComponent } from './pages/home-page/home-page.component';
-import { ProfilePageComponent } from './pages/profile-page/profile-page.component';
 
 const routes: Routes = [
-  {path:'home', component:HomePageComponent},
-  {path:'courses', component:CoursesPageComponent},
-  {path:'about-us', component:AboutUsButtonComponent},
-  {path:'profile',component:ProfilePageComponent},
-  {path:'**', redirectTo: 'home', pathMatch: 'full'},
-  {path:'', component:HomePageComponent}
+  {
+    path: 'home', component: HomePageComponent,
+    children: [
+      { path: 'course/:id', component: CoursesComponent }
+    ]
+  },
+  { path: 'courses', component: CoursesPageComponent },
+  { path: 'about-us', component: AboutUsButtonComponent },
+  { path: '**', redirectTo: 'home', pathMatch: 'full' },
+  { path: '', component: HomePageComponent }
 ];
 
 @NgModule({
