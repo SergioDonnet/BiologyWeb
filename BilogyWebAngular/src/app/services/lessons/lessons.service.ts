0fc1c0e12c679bd18fc323b46f00b5e613ad4672
GabyMarsan <anagbms07@gmail.com>
2022-10-29 02:10:37 +0000

    lessons get, list

--- BilogyWebAngular/src/app/services/lessons/lessons.service.ts
+++ BilogyWebAngular/src/app/services/lessons/lessons.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@angular/core';
-import { collection, Firestore, getDocs } from '@angular/fire/firestore';
-import { query, where } from 'firebase/firestore';
+import { collection, Firestore, getDoc, getDocs, addDoc, deleteDoc, setDoc, doc, docData, query, where, DocumentData, DocumentSnapshot, QueryDocumentSnapshot } from '@angular/fire/firestore';
 import { Observable } from 'rxjs';
 
 @Injectable({
@@ -10,6 +9,37 @@ export class LessonsService {
 
   constructor(private db: Firestore) { }
 
+  async createLesson(lesson: Lesson) {
+    return addDoc(collection(this.db, `lessons/${lesson.id}`), lesson);
+  }
+
+  async getLesson(id: string) {
+    const fbObject = await getDoc(doc(this.db, `lessons/${id}`));
+    //const lesson: Lesson = this.
+  }
+
+  async updateLesson(lesson: Lesson) {
+    return setDoc(doc(this.db, `lessons/${lesson.id}`), lesson, { merge: true });
+  }
+
+  async listLessons() {
+    const firebaseObjects = await getDocs(collection(this.db, "lessons"));
+
+    const lessons: Lesson[] = [];
+    firebaseObjects.forEach((fbObject) => {
+      lessons.push(this.convertLesson(fbObject)!);
+    });
+    return lessons;
+  }
+
+  async deleteLesson(lesson: Lesson) {
+    return deleteDoc(doc(this.db, `lessons/${lesson.id}`));
+  }
+
+  async getProgressLesson() {
+
+  }
+
   async loadLessonsForCourse(courseId: string) {
     const q = query(collection(this.db, "lessons"), where("courseId", "==", courseId));
     const querySnapshot = await getDocs(q);
@@ -17,28 +47,32 @@ export class LessonsService {
     querySnapshot.forEach((doc) => {
       const data = doc.data();
       lessons.push({
-        curseId: data['curseId'],
+        courseId: data['courseId'],
         title: data['title'],
+        id: doc.id
       });
     });
     return lessons;
   }
 
-  async loadLessons() {
-    const querySnapshot = await getDocs(collection(this.db, "lessons"));
-    const lessons: Lesson[] = [];
-    querySnapshot.forEach((doc) => {
-      const data = doc.data();
-      lessons.push({
-        curseId: data['curseId'],
-        title: data['title'],
-      });
-    });
-    return lessons;
+  
+
+  private convertLesson(doc: DocumentSnapshot<DocumentData>) {
+    const data = doc.data();
+    if (!data) {
+      return undefined;
+    }
+    return {
+      id: doc.id,
+      title: data['title'],
+      courseId: data['courseId']
+    };
   }
 }
 
+
 export interface Lesson {
-  curseId: string,
+  courseId: string,
   title: string,
+  id: string
 };
