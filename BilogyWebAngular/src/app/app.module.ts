895d0872744a5c415862a2c164f7b8f8cc8b9ad9
AnaMarsan <ana_gaby2702@hotmail.com>
2022-11-24 01:07:10 +0000

    about us formato pagina

--- BilogyWebAngular/src/app/app.module.ts
+++ BilogyWebAngular/src/app/app.module.ts
@@ -9,7 +9,7 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatInputModule } from '@angular/material/input';
-import { MatGridListModule } from '@angular/material/grid-list';
+import { MatMenuModule } from '@angular/material/menu';
 import { CoolSocialLoginButtonsModule } from '@angular-cool/social-login-buttons';
 import { ReactiveFormsModule } from '@angular/forms';
 import { LoginComponent } from './components/login/login.component';
@@ -68,7 +68,7 @@ import { CoursesPageSeresVivosComponent } from './pages/courses-page/courses-pag
     MatInputModule,
     ReactiveFormsModule,
     CoolSocialLoginButtonsModule,
-    MatGridListModule,
+    MatMenuModule,
 
     provideFirebaseApp(() => initializeApp(environment.firebase)),
     provideAuth(() => getAuth()),
