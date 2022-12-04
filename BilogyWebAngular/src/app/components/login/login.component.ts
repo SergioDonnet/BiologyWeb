f2827bfa2f877f677ae2837ba42ea032de300f7b
SergioDonnet <donnet7534@gmail.com>
2022-12-04 21:27:15 +0000

    autenticacion login

--- BilogyWebAngular/src/app/components/login/login.component.ts
+++ BilogyWebAngular/src/app/components/login/login.component.ts
@@ -1,32 +1,43 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormControl, FormGroup } from '@angular/forms';
+import { AuthService } from 'src/app/services/auth/auth.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
+
 })
 export class LoginComponent implements OnInit {
 
-  hide: boolean = false;
+  formLogin: FormGroup;
 
-  constructor(private fb: FormBuilder) { 
-
-  }
-
-  ngOnInit(){
-  }
-
-  loginForm: FormGroup = this.fb.group({
-    email: ['',[Validators.required, Validators.email]],
-    password: ['', [Validators.required, Validators.minLength(6)]]
+  constructor(private authService:AuthService) { 
+  this.formLogin = new FormGroup({
+    email:new FormControl(),
+    password:new FormControl()
   })
+  }
 
-  onLogin(){
-    if (!this.loginForm.valid){
-      return;
-    }
-    console.log(this.loginForm.value);
+  ngOnInit():void{
+  }
+
+  onSumit(){
+    this.authService.login(this.formLogin.value)
+    .then(response =>{
+      console.log(response);
+    })
+    .catch(error => console.log(error));
+  }
+
+  onClick(){
+    this.authService.loginWithGoogle()
+    .then(response =>{
+
+    })
+    .catch(error => console.log(error));
   }
 
 }
+
+
