91f239dcfdcb71e800e7a9bffc14ba3dc8a0872a
AnaMarsan <ana_gaby2702@hotmail.com>
2022-12-07 08:05:03 +0000

    auth logOut

--- BilogyWebAngular/src/app/pages/profile-page/profile-page.component.ts
+++ BilogyWebAngular/src/app/pages/profile-page/profile-page.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/services/auth/auth.service';
 
 @Component({
   selector: 'app-profile-page',
@@ -7,9 +9,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfilePageComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private authService: AuthService,
+    private router: Router
+    ) { }
 
   ngOnInit(): void {
+  }
+
+  onClick(){
+    this.authService.logOut()
+    .then(()=>{
+      this.router.navigate(['/courses'])
+    })
+    .catch(error=>console.log(error));
   }
 
 }
