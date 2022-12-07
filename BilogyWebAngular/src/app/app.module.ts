f301b0f1f076234478d6dcfd42f60e9eddc49292
SergioDonnet <donnet7534@gmail.com>
2022-12-07 09:20:52 +0000

    se crea componente curse1lesson1page5 html css

--- BilogyWebAngular/src/app/app.module.ts
+++ BilogyWebAngular/src/app/app.module.ts
@@ -37,6 +37,7 @@ import { Course1lesson1page1Component } from './coursesgo/course1/lesson1/page1/
 import { Course1lesson1page2Component } from './coursesgo/course1/lesson1/page2/course1lesson1page2/course1lesson1page2.component';
 import { Course1lesson1page3Component } from './coursesgo/course1/lesson1/page3/course1lesson1page3/course1lesson1page3.component';
 import { Course1lesson1page4Component } from './coursesgo/course1/lesson1/page4/course1lesson1page4/course1lesson1page4.component';
+import { Course1lesson1page5Component } from './coursesgo/course1/lesson1/page5/course1lesson1page5/course1lesson1page5.component';
 //import { CoursesInfoComponent } from './components/courses-info/courses-info.component';
 
 @NgModule({
@@ -64,6 +65,7 @@ import { Course1lesson1page4Component } from './coursesgo/course1/lesson1/page4/
        Course1lesson1page2Component,
        Course1lesson1page3Component,
        Course1lesson1page4Component,
+       Course1lesson1page5Component,
     //CoursesInfoComponent,
   ],
   imports: [
---
+++ BilogyWebAngular/src/app/coursesgo/course1/lesson1/page5/course1lesson1page5/course1lesson1page5.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-course1lesson1page5',
+  templateUrl: './course1lesson1page5.component.html',
+  styleUrls: ['./course1lesson1page5.component.scss']
+})
+export class Course1lesson1page5Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
